6ee3a685a58f6a905dd45de24937ff1706470b66
Kushagra <kushagragarg1998@gmail.com>
2024-01-09 17:58:41 +0530

    feat: working on authentication

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,3 +1,15 @@
 export const TOO_MANY_REQUEST =
   'Too many requests from this IP, Please try after sometime';
 export const DB_CONNECT_SUCCESS = 'DB connection successful';
+export const USER_SCHEMA_VALIDATION = {
+  firstName: 'A first name is required',
+  lastName: 'A first name is required',
+  email: 'A email is required',
+  phone: 'A phone number is required',
+  invalidEmail: 'Invalid email id',
+  brand: 'A brand is required',
+  password: 'A password is required',
+  mismatchPasswords: 'Password and Confirm password did not match.',
+};
+export const brandEnum = ['pinch', 'bob'];
+export const roleEnum = ['admin', 'customer', 'staff'];
