f962ae3953f9e3249bd88012c1ba395574f19054
Kushagra <kushagragarg1998@gmail.com>
2024-02-15 16:54:29 +0530

    feat: product api development

---
+++ src/models/categoryModel.ts
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+import { brandEnum } from '@src/types/customTypes';
+import { CATEGORY_SCHEMA_VALIDATION } from '@src/constants/messages';
+
+interface ICategory {
+  brand: string;
+  name: string;
+}
+
+const categorySchema = new mongoose.Schema<ICategory>(
+  {
+    brand: {
+      type: String,
+      required: [true, CATEGORY_SCHEMA_VALIDATION.brand],
+      enum: brandEnum,
+    },
+    name: {
+      type: String,
+      required: [true, CATEGORY_SCHEMA_VALIDATION.name],
+      unique: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Category = mongoose.model('Category', categorySchema);
+
+export default Category;
--- src/models/productModel.ts
+++ src/models/productModel.ts
@@ -1,37 +1,120 @@
-import mongoose, { Types } from 'mongoose';
+import mongoose, { Types, model } from 'mongoose';
+import { brandEnum, typeEnum } from '@src/types/customTypes';
+import { PRODUCT_SCHEMA_VALIDATION } from '@src/constants/messages';
 
-interface ICategory {
-  _id: mongoose.ObjectId;
+interface ISize {
+  name: string;
+  price: number;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+interface IPhoto {
+  key: string;
+  name: string;
+  mimeType: string;
+  size: number;
+  url: string;
+}
+
+interface IProductDetails {
+  details: string;
+  dietaryAdvice: string;
+  shelfLife: string;
+  sizesAndPortions: string;
+}
+
 interface IProduct {
   name: string;
   price: number;
-  discountedPrice: number;
+  discountedPrice?: number;
   currency: string;
   brand: string;
-  pieces: number;
-  size: string[];
-  images: string[];
+  pieces?: number;
+  size?: ISize[];
+  images: IPhoto[];
   flavour: string[];
-  type: string;
-  details: {
-    details: string;
-    dietaryAdvice: string;
-    shelfLife: string;
-    sizesAndPortions: string;
-  };
+  type: string; // cake or bake ?
+  details: IProductDetails;
   maxQty: number;
   recommended: boolean;
   active: boolean;
-  category: Types.DocumentArray<ICategory>;
-  fbt: string[];
+  category: Types.ObjectId;
+  fbt: string[]; // frequently bought together
 }
 
-// const productSchema = new mongoose.Schema<IProduct>({
-//   name: {
-//     type: String,
-//     required: [true, 'A product must have a name'],
-//   },
-// });
+const ProductImageSchema = new mongoose.Schema({
+  key: String,
+  name: String,
+  mimeType: String,
+  size: Number,
+  url: String,
+});
+
+const ProductDetailSchema = new mongoose.Schema({
+  details: String,
+  dietaryAdvice: String,
+  shelfLife: String,
+  sizesAndPortions: String,
+});
+
+const productSchema = new mongoose.Schema<IProduct>({
+  name: {
+    type: String,
+    trim: true,
+    required: [true, PRODUCT_SCHEMA_VALIDATION.name],
+  },
+  price: {
+    type: Number,
+    required: [true, PRODUCT_SCHEMA_VALIDATION.price],
+  },
+  discountedPrice: Number,
+  currency: {
+    type: String,
+    required: [true, PRODUCT_SCHEMA_VALIDATION.currency],
+  },
+  brand: {
+    type: String,
+    required: [true, PRODUCT_SCHEMA_VALIDATION.brand],
+    enum: brandEnum,
+  },
+  pieces: Number,
+  size: [
+    {
+      name: String,
+      price: Number,
+    },
+  ],
+  images: {
+    type: [ProductImageSchema],
+    required: [true, PRODUCT_SCHEMA_VALIDATION.images],
+  },
+  flavour: [String],
+  type: {
+    type: String,
+    required: [true, PRODUCT_SCHEMA_VALIDATION.type],
+    enum: typeEnum,
+  },
+  details: {
+    type: ProductDetailSchema,
+    required: [true, PRODUCT_SCHEMA_VALIDATION.detail],
+  },
+  maxQty: Number,
+  recommended: {
+    type: Boolean,
+    default: false,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: [true, PRODUCT_SCHEMA_VALIDATION.category],
+  },
+  fbt: [String],
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
+});
+
+const Product = model<IProduct>('Product', productSchema);
+
+export default Product;
