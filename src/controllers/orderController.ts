fc0819c64a3a8c90a0be459ac03850834e36bb4f
Kushagra <kushagragarg1998@gmail.com>
2024-03-29 14:46:42 +0530

    feat: working on stripe + address API changes

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -1,15 +1,13 @@
 /* eslint-disable import/prefer-default-export */
 import { Response } from 'express';
 import catchAsync from '@src/utils/catchAsync';
-import Order from '@src/models/orderModel';
+// import Order from '@src/models/orderModel';
 import { IRequestWithUser } from './authController';
 
 export const placeOrder = catchAsync(
   async (req: IRequestWithUser, res: Response) => {
     req.body.user = req.user?._id;
-    console.log(req.body, 'req.body');
-    const doc = await Order.create(req.body);
-    console.log(doc, '1111');
+    // const doc = await Order.create(req.body);
     res.send(200);
   }
 );
