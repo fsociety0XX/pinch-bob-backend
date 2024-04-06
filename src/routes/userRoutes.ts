5a99eaec1d5473de6f7bb7149c2777f9e9206391
Kushagra <kushagragarg1998@gmail.com>
2024-04-06 15:28:07 +0530

    feat: working on API optimisation

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,41 +1,18 @@
 import express from 'express';
+import { protect, roleRistriction } from '@src/controllers/authController';
+import { Role } from '@src/types/customTypes';
 import {
-  changePassword,
-  forgotPassword,
-  protect,
-  resetPassword,
-  sendOtp,
-  signin,
-  signup,
-  verifyOtp,
-} from '@src/controllers/authController';
-import uploadImage from '@src/utils/uploadImage';
-import {
-  CHANGE_PASSWORD,
-  FORGOT_PASSWORD,
-  RESET_PASSWORD,
-  SEND_OTP,
-  SIGN_IN,
-  SIGN_UP,
-  VERIFY_OTP,
-} from '@src/constants/routeConstants';
+  createUser,
+  deleteUser,
+  getAllUser,
+  getOneUser,
+  updateUser,
+} from '@src/controllers/userController';
 
 const userRouter = express.Router();
+userRouter.use(protect, roleRistriction(Role.ADMIN));
 
-// Auth routes
-
-userRouter.post(SEND_OTP, sendOtp);
-userRouter.post(VERIFY_OTP, verifyOtp);
-userRouter.post(
-  SIGN_UP,
-  uploadImage(process.env.AWS_BUCKET_PROFILE_PATH!).single('photo'),
-  signup
-);
-userRouter.post(SIGN_IN, signin);
-userRouter.post(FORGOT_PASSWORD, forgotPassword);
-userRouter.patch(RESET_PASSWORD, resetPassword);
-
-// User routes
-userRouter.patch(CHANGE_PASSWORD, protect, changePassword);
+userRouter.route('/').get(getAllUser).post(createUser);
+userRouter.route('/:id').get(getOneUser).patch(updateUser).delete(deleteUser);
 
 export default userRouter;
