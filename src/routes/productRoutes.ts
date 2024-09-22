52c2e8e8577d2fc3fbf50a981b5cc7cefff7f361
vincox <vincox@gmail.com>
2024-09-22 21:54:43 +0800

    feat: worked on global search for products

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -7,11 +7,14 @@ import {
   getOneProduct,
   getOneProductViaSlug,
   updateProduct,
+  globalSearch,
 } from '@src/controllers/productController';
 import { Role } from '@src/types/customTypes';
 import uploadImage from '@src/utils/uploadImage';
 
 const productRouter = express.Router();
+
+productRouter.route('/globalSearch').get(globalSearch);
 
 productRouter
   .route('/')
