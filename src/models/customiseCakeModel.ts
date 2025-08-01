9202cc1e3899e889b43548f594c75022888464b1
Kushagra <kushagragarg1998@gmail.com>
2025-08-01 18:06:40 +0530

    feat: add moneyPaid related fields to customiseCake and order

--- src/models/customiseCakeModel.ts
+++ src/models/customiseCakeModel.ts
@@ -121,6 +121,8 @@ export interface ICustomiseCake {
   candlesAndSparklers: ICandles[];
   isMoneyPulling: boolean;
   moneyPulling?: IMoneyPulling[];
+  moneyPaidForMoneyPulling?: boolean;
+  moneyReceivedForMoneyPulling?: boolean;
   price: number;
   quantity: number;
   size: string;
@@ -317,6 +319,14 @@ const customiseCakeSchema = new mongoose.Schema<ICustomiseCake>(
       default: false,
     },
     moneyPulling: [MoneyPullingSchema],
+    moneyPaidForMoneyPulling: {
+      type: Boolean,
+      default: false,
+    },
+    moneyReceivedForMoneyPulling: {
+      type: Boolean,
+      default: false,
+    },
     deliveryFee: Number,
     discountedAmt: Number,
     total: Number,
