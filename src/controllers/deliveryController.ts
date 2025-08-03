f17ca02fad4a13a5b82ca3859fa3924be94c6e84
Kushagra <kushagragarg1998@gmail.com>
2025-08-03 15:22:54 +0530

    feat: added driver details in orders

--- src/controllers/deliveryController.ts
+++ src/controllers/deliveryController.ts
@@ -181,6 +181,10 @@ export const updateOrderStatus = catchAsync(
     );
     await Order.findByIdAndUpdate(order?._id, {
       status: WOODELIVERY_STATUS[req.body.StatusId],
+      driverDetails: {
+        id: req.body.assignedToDriverUserId,
+        name: req.body.driverName,
+      },
     });
 
     // Send order delivered email and ask for google review from cx
